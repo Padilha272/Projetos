9fe286c6f0316b55f24cc6f87c0858eefdc94f15
Matheus de Araújo Padilha <mapadilha02@gmail.com>
2023-10-31 14:19:07 -0300

    Insert Method

--- TestSpring/src/main/java/com/example/demo/resources/AnimalResource.java
+++ TestSpring/src/main/java/com/example/demo/resources/AnimalResource.java
@@ -1,14 +1,20 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.models.Animal;
 import com.example.demo.services.AnimalService;
@@ -29,5 +35,18 @@ public class AnimalResource {
 		response.put("animals", list);
 		return ResponseEntity.ok().body(response);
 	}
+	
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Optional<Animal>> findById(@PathVariable Integer id) {
+		Optional<Animal> animal = animalService.findById(id);
+		return ResponseEntity.ok().body(animal);
+	}
 
+	@PostMapping
+	public ResponseEntity<Animal> insert (@RequestBody Animal obj){
+		obj = animalService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
--- TestSpring/src/main/java/com/example/demo/services/AnimalService.java
+++ TestSpring/src/main/java/com/example/demo/services/AnimalService.java
@@ -1,9 +1,11 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.example.demo.models.Animal;
 import com.example.demo.repositories.AnimalRepository;
@@ -17,6 +19,15 @@ public class AnimalService {
 	public List<Animal> getAllAnimals(){
 		List<Animal>list=animalRepository.findAll();
 		return list;
+	}
+	
+	public Optional<Animal> findById(Integer Id) {
+		Optional<Animal> animal = animalRepository.findById(Id);
+		return animal;		
+	}
+	
+	public Animal insert(Animal obj) {
+		return animalRepository.save(obj);
 	}
 
 }
